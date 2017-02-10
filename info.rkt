ccb4fc90498b1fadeb96cca656e8e6d7adf2a85b
Leandro Facchinetti <me@leafac.com>
2017-02-10 10:03:11 -0500

    Add missing dependency

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "css-expr")
 (define version "0.0.1")
 (define deps '("base" "rackunit-lib" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
 (define scribblings '(("documentation/css-expr.scrbl" ())))
 (define compile-omit-paths '("tests"))
 (define pkg-desc "S-expression-based CSS")
