ad26bd81065f2f9406ad0ddc62b9d23ce04cf523
Leandro Facchinetti <me@leafac.com>
2018-05-09 07:24:58 -0400

    Move documentation

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define version "0.0.2")
 (define deps '("base" "rackunit-lib" "typed-racket-lib" "typed-racket-more" "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
-(define scribblings '(("documentation/css-expr.scrbl" ())))
+(define scribblings '(("css-expr.scrbl" ())))
 (define pkg-desc "S-expression-based CSS")
 (define pkg-authors '(leafac))
